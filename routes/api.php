c2a02c6a2b82f79f5cc66d3e5094bb53ef06fc94
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-12-31 00:33:19 +0100

    fix : add a message on run api

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::get('/', function () {
-    return 'API';
+    return 'API is running';
 });
 
 //public auth
