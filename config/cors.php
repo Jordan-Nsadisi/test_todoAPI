f15b91ad6559987720ff4bfdec97af6eea39c9cb
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-12-31 06:57:37 +0100

    fix cors origin for dev mode

--- config/cors.php
+++ config/cors.php
@@ -25,27 +25,9 @@ return [
    'allowed_methods' => ['*'],
 
    'allowed_origins' => [
-      //utilisation des variables d'environnement
-      env('FRONTEND_URL', 'http://localhost:3000'),
-
-      // Next.js dev mode
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-
-
-      //react Native dev mode
-      'http://localhost:8081',
-      'http://127.0.0.1:8081',
-
-      //expo Development
-      'http://localhost:19006',
-      'http://127.0.0.1:19006',
-      'exp://localhost:19000',
-      'exp://127.0.0.1:19000',
-
-      //local mode dev mobile
-      'ajouter api local en dev mobile',
-   ],
+      //dev mode, accepter toutes les origines
+      env('CORS_ALLOWED_ORIGINS') === '*' ? '*' : explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000'))
+   ][0] === '*' ? ['*'] : explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000')),
 
    'allowed_origins_patterns' => [
       // Pour les tunnels Expo et ngrok
